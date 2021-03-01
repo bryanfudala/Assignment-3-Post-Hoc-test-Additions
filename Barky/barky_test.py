6b9e8a7f2f571cea42d832d9090d7146428343d7
bryanfudala <btfudala1@buffs.wtamu.edu>
2021-02-28 19:28:46 -0700

    done

---
+++ Barky/barky_test.py
@@ -0,0 +1,34 @@
+import pytest
+import commands
+import barky
+from database import DatabaseManager
+
+
+db=DatabaseManager('bookmarks.db')
+
+# test adding bookmark
+
+def AddBookMarkTest():
+    expecteddata = {'title':'Mock Title', 'url': 'Mock Url', 'notes':'Mock Notes'}
+    db.add('bookmarks', expecteddata)
+    assert db.add.bookmark('Mock Title') > 0
+
+
+# test removing bookmark
+
+def RemoveBookMarkTest():
+     expecteddata = {'title':'Mock Title', 'url': 'Mock Url', 'notes':'Mock Notes'}
+     db.add('bookmarks', expecteddata)
+     RemoveRecord = {'title':'Mock Title'}
+     db.delete('bookmarks_Test',RemoveRecord)
+     assert db.bookmark('Mock Title') < 0
+
+# test to validate list by title (in progress)
+#def ListBookMarkTest():
+   # db.select(commands.ListBookmarksCommand {'id': 3}
+    #assert expecteddata == pulleddata
+
+
+
+
+
