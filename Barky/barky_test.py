13f06d5923f769189a3b2bf08ac1bbd2deddc9ac
bryanfudala <btfudala1@buffs.wtamu.edu>
2021-03-03 18:15:31 -0700

    update

--- Barky/barky_test.py
+++ Barky/barky_test.py
@@ -1,15 +1,17 @@
 import pytest
 import commands
-import barky
+#import barky
 from database import DatabaseManager
 
 
-db=DatabaseManager('bookmarks.db')
+db = DatabaseManager('bookmarks.db')
 
 # test adding bookmark
 
+
 def AddBookMarkTest():
-    expecteddata = {'title':'Mock Title', 'url': 'Mock Url', 'notes':'Mock Notes'}
+    expecteddata = {'title': 'Mock Title',
+                    'url': 'Mock Url', 'notes': 'Mock Notes'}
     db.add('bookmarks', expecteddata)
     assert db.add.bookmark('Mock Title') > 0
 
@@ -17,18 +19,14 @@ def AddBookMarkTest():
 # test removing bookmark
 
 def RemoveBookMarkTest():
-     expecteddata = {'title':'Mock Title', 'url': 'Mock Url', 'notes':'Mock Notes'}
-     db.add('bookmarks', expecteddata)
-     RemoveRecord = {'title':'Mock Title'}
-     db.delete('bookmarks_Test',RemoveRecord)
-     assert db.bookmark('Mock Title') < 0
+    expecteddata = {'title': 'Mock Title',
+                    'url': 'Mock Url', 'notes': 'Mock Notes'}
+    db.add('bookmarks', expecteddata)
+    RemoveRecord = {'title': 'Mock Title'}
+    db.delete('bookmarks_Test', RemoveRecord)
+    assert db.bookmark('Mock Title') < 0
 
 # test to validate list by title (in progress)
-#def ListBookMarkTest():
-   # db.select(commands.ListBookmarksCommand {'id': 3}
+# def ListBookMarkTest():
+  # db.select(commands.ListBookmarksCommand {'id': 3}
     #assert expecteddata == pulleddata
-
-
-
-
-
